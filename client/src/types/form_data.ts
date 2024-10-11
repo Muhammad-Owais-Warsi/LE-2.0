174275c7ca430e59b353761f4d9fcec8ea59d54d
Muhammad-Owais-Warsi <warsimuhammadowais@gmail.com>
2024-10-11 16:15:14 +0530

    leave form

---
+++ client/src/types/form_data.ts
@@ -0,0 +1,21 @@
+
+
+export interface form_data {
+    StudentImage: string;
+    StudentName: string;
+    StudentEmail: string;
+    StudentRegisterNumber:string;
+    StudentPhoneNumber: string;
+    ParentPhoneNumber: string;
+    HostelName: string;
+    HostelRoomNumber: string;
+    DateOut: string
+    DateIn: string;
+    TimeOut: string;
+    TimeIn: string;
+    Reason: string;
+    CollegeName: string;
+    Department: string;
+    Section: string;
+    LeaveProof: string;
+}
